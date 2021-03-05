0040087693aa9901fb41c2e4cd109d606dbb6ba4
jefferyl <clay615040@hotmail.com>
2021-03-05 16:11:31 +0800

    再測試

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
-<template>
-  tesesese
+<template lang="pug">
+.background
+  img.background__img(src="./images/childblue.jpg")
+  .ball
+    .level__one(@mouseover="changeLevelOne()")
+      .level__two(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo()" :key="i")
+        //- .level__three(v-for="(item, i) in 4" @mouseover="changeLevelThree()" :key="i")
+          //- .level__four(v-for="(item, i) in 4" @mouseover="changeLevelFour()" :key="i")
 </template>
-
 <script>
-
 export default {
-  name: 'img__ball',
+  name: 'imgball',
+
+  data() {
+    return {
+      levelOne: true,
+      levelTwo: [0, 0, 0, 0],
+      levelThree: [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]],
+    }
+  },
+
+  mounted () {
+    document.body.style.margin = 0
+  },
+
+  methods: {
+    changeLevelOne () {
+      this.levelOne = false
+      this.levelTwo = true
+    },
+    // changeLevelTwo () {
+    //   this.levelTwo = false
+    //   this.levelThree = true
+    // },
+  }
 }
 </script>
+<style lang="scss" scoped>
+  @import './style.scss';
+</style>
