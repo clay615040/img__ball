22f3ffd8475d90fc882015cfa8bb927adbb9e3a9
jefferyl <clay615040@hotmail.com>
2021-03-11 14:53:20 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 .background
   img.background__img(src="./images/childblue.jpg")
   .ball
-    .level__one(@mouseover="changeLevelOne()")
-      .level__two(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo()" :key="i")
-        //- .level__three(v-for="(item, i) in 4" @mouseover="changeLevelThree()" :key="i")
-          //- .level__four(v-for="(item, i) in 4" @mouseover="changeLevelFour()" :key="i")
+    .level__one(@mouseover="changeLevelOne()" :class="{active: levelOne}")
+    .level__two__group
+      .level__two(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo(i)" :class="{active: levelOne === levelTwo[i]}" :key="i")
+    //- .level__three(v-for="(item, i) in 4" @mouseover="changeLevelThree()" :key="i")
+      //- .level__four(v-for="(item, i) in 4" @mouseover="changeLevelFour()" :key="i")
 </template>
 <script>
 export default {
@@ -13,9 +14,9 @@ export default {
 
   data() {
     return {
-      levelOne: true,
-      levelTwo: [0, 0, 0, 0],
-      levelThree: [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]],
+      levelOne: false,
+      levelTwo: [false, false, false, false],
+      // levelThree: [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]],
     }
   },
 
@@ -25,13 +26,14 @@ export default {
 
   methods: {
     changeLevelOne () {
-      this.levelOne = false
-      this.levelTwo = true
+      console.log('levelOne')
+      this.levelOne = true
     },
-    // changeLevelTwo () {
-    //   this.levelTwo = false
-    //   this.levelThree = true
-    // },
+    changeLevelTwo (i) {
+      console.log('levelTwo')
+      console.log(i)
+      this.levelTwo[i] = true
+    },
   }
 }
 </script>
