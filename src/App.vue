fcd9f9890af2388bbd137bb9829ece43792ec037
jefferyl <clay615040@hotmail.com>
2021-03-16 17:25:20 +0800

    第四層

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     .level__one(@mouseover="changeLevelOne()" :class="{active: levelOne}")
       .level__two(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo(i)" :class="{active: levelOne === levelTwo[i]}" :key="i")
         .level__three(v-for="(item, i2) in levelThree[i]" @mouseover="changeLevelThree(i,i2)" :class="{active: !levelTwo[i] || levelThree[i][i2]}" :key="i2")
+          .level__four(v-for="(item, i3) in levelFour[i][i2]" @mouseover="changeLevelFour(i,i2,i3)" :class="{active: !levelThree[i][i2] || levelFour[i][i2][i3]}" :key="i3")
 </template>
 <script>
 export default {
@@ -14,7 +15,18 @@ export default {
     return {
       levelOne: false,
       levelTwo: [false, false, false, false],
-      levelThree: [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]],
+      levelThree: [
+        [false, false, false, false],
+        [false, false, false, false], 
+        [false, false, false, false], 
+        [false, false, false, false]
+      ],
+      levelFour: [
+        [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]],
+        [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], 
+        [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], 
+        [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]]
+      ],
     }
   },
 
@@ -24,22 +36,17 @@ export default {
 
   methods: {
     changeLevelOne () {
-      // console.log('levelOne')
       this.levelOne = true
     },
     changeLevelTwo (i) {
-      console.log('levelTwo')
-      // console.log(i)
       this.levelTwo[i] = true
     },
     changeLevelThree (i, i2) {
-      console.log('levelThree')
-      console.log(i)
-      console.log(i2)
       this.levelThree[i][i2] = true
     },
-    test () {
-      console.log('levelThree')
+    changeLevelFour (i, i2, i3) {
+      console.log(i, i2, i3)
+      this.levelFour[i][i2][i3] = true
     },
   }
 }
