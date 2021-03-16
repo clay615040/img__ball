6f6ed277fd9bbd513dcc1c42e73a68efea055c4b
jefferyl <clay615040@hotmail.com>
2021-03-16 17:14:25 +0800

    修正bug

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,8 @@
   img.background__img(src="./images/childblue.jpg")
   .ball
     .level__one(@mouseover="changeLevelOne()" :class="{active: levelOne}")
-    .level__two__group
       .level__two(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo(i)" :class="{active: levelOne === levelTwo[i]}" :key="i")
-    .level__three__group
-      .three(v-for="(node, i) in levelThree" :class="{active: !levelOne || !levelTwo[i]}")
-        .level__three(v-for="(item, i2) in node" @mouseover="changeLevelThree(i,i2)" :class="{active: levelThree[i][i2]}" :key="i2")
+        .level__three(v-for="(item, i2) in levelThree[i]" @mouseover="changeLevelThree(i,i2)" :class="{active: !levelTwo[i] || levelThree[i][i2]}" :key="i2")
 </template>
 <script>
 export default {
@@ -27,12 +24,12 @@ export default {
 
   methods: {
     changeLevelOne () {
-      console.log('levelOne')
+      // console.log('levelOne')
       this.levelOne = true
     },
     changeLevelTwo (i) {
       console.log('levelTwo')
-      console.log(i)
+      // console.log(i)
       this.levelTwo[i] = true
     },
     changeLevelThree (i, i2) {
@@ -40,6 +37,9 @@ export default {
       console.log(i)
       console.log(i2)
       this.levelThree[i][i2] = true
+    },
+    test () {
+      console.log('levelThree')
     },
   }
 }
