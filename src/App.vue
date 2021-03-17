a6098c3866a797b0e772230acbbbaa68596ff31d
jefferyl <clay615040@hotmail.com>
2021-03-17 15:23:01 +0800

    第五層

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 .background
   img.background__img(src="./images/childblue.jpg")
   .ball
-    .level__one(@mouseover="changeLevelOne()" :class="{active: levelOne}")
-      .level__two(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo(i)" :class="{active: levelOne === levelTwo[i]}" :key="i")
-        .level__three(v-for="(item, i2) in levelThree[i]" @mouseover="changeLevelThree(i,i2)" :class="{active: !levelTwo[i] || levelThree[i][i2]}" :key="i2")
-          .level__four(v-for="(item, i3) in levelFour[i][i2]" @mouseover="changeLevelFour(i,i2,i3)" :class="{active: !levelThree[i][i2] || levelFour[i][i2][i3]}" :key="i3")
+    .extremely(@mouseover="changeLevelOne()" :class="{active: levelOne}")
+      .ball__bg(v-for="(item, i) in levelTwo" @mouseover="changeLevelTwo(i)" :class="{active: levelOne === levelTwo[i]}" :key="i")
+        .ball__bg(v-for="(item, i2) in levelThree[i]" @mouseover="changeLevelThree(i,i2)" :class="{active: !levelTwo[i] || levelThree[i][i2]}" :key="i2")
+          .ball__bg(v-for="(item, i3) in levelFour[i][i2]" @mouseover="changeLevelFour(i,i2,i3)" :class="{active: !levelThree[i][i2] || levelFour[i][i2][i3]}" :key="i3")
+            .ball__bg(v-for="(item, i4) in levelFive[i][i2][i3]" @mouseover="changeLevelFive(i,i2,i3,i4)" :class="{active: !levelFour[i][i2][i3] || levelFive[i][i2][i3][i4]}" :key="i4")
 </template>
 <script>
 export default {
@@ -27,6 +28,12 @@ export default {
         [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], 
         [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]]
       ],
+      levelFive: [
+        [[[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]]],
+        [[[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]]], 
+        [[[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]]], 
+        [[[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]], [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]]]
+      ],
     }
   },
 
@@ -45,8 +52,10 @@ export default {
       this.levelThree[i][i2] = true
     },
     changeLevelFour (i, i2, i3) {
-      console.log(i, i2, i3)
       this.levelFour[i][i2][i3] = true
+    },
+    changeLevelFive (i, i2, i3, i4) {
+      this.levelFive[i][i2][i3][i4] = true
     },
   }
 }
